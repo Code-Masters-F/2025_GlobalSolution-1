5f3ecdcadccbc2c17ec7230caeda924f7ca7cf20
Guilherme <guilhermeavellar25@gmail.com>
2025-06-05 18:34:47 -0300

    feat: List for all AbrigosImprovisados and toString

--- back-end/src/AbrigoOficial.java
+++ back-end/src/AbrigoOficial.java
@@ -1,4 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class AbrigoOficial extends Abrigo{
+
+    public static Set<AbrigoOficial> todosAbrigosOficiais = new HashSet<>();
+
+    public static void exibir() {
+        for(AbrigoOficial abrigo : todosAbrigosOficiais) {
+            System.out.println(abrigo);
+        }
+    }
+
+    public static void remover(int id) {
+        for(AbrigoOficial abrigoOficial : todosAbrigosOficiais) {
+            if(abrigoOficial.getId() == id) {
+                todosAbrigosOficiais.remove(abrigoOficial);
+                break;
+            }
+        }
+    }
+
     private String nome;
     private String cnpj;
 
@@ -9,6 +30,8 @@ public class AbrigoOficial extends Abrigo{
         super(id, cep, endereco, email, dataFuncionamento, capacidadeMaxima);
         this.nome = nome;
         this.cnpj = cnpj;
+
+        todosAbrigosOficiais.add(this);
     }
 
     public String getNome() {
@@ -17,5 +40,10 @@ public class AbrigoOficial extends Abrigo{
 
     public String getCNPJ() {
         return cnpj;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Nome: " + this.nome + " - CNPJ: " + this.cnpj;
     }
 }

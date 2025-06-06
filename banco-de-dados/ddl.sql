348045dea40c8075097ceaebf4f0024a13f32cca
Guilherme <guilhermeavellar25@gmail.com>
2025-06-06 20:05:01 -0300

    feat: add attributes

--- banco-de-dados/ddl.sql
+++ banco-de-dados/ddl.sql
@@ -3,7 +3,7 @@
 CREATE TABLE Usuario (
     id INT PRIMARY KEY,
     cpf VARCHAR (18) NOT NULL UNIQUE , 
-    nome VARCHAR (100) NOT NULL UNIQUE,
+    nome VARCHAR (100) NOT NULL,
     email VARCHAR(300) NOT NULL UNIQUE,
     senha VARCHAR (100) NOT NULL,
     telefone VARCHAR (20) NOT NULL
@@ -14,8 +14,11 @@ CREATE TABLE AbrigoOficial (
     nome VARCHAR (250) NOT NULL,
     cnpj VARCHAR (18) NOT NULL UNIQUE,
     endereco VARCHAR (300) NOT NULL,
-    cep VARCHAR(8) NOT NULL,
-    capacidade INT DEFAULT 0
+    cep VARCHAR(9) NOT NULL,
+    email VARCHAR(300),
+    telefone VARCHAR (20) NOT NULL,
+    capacidadeMaxima INT DEFAULT 0,
+    dataFuncionamento VARCHAR (16) NOT NULL
 );
 
 CREATE TABLE AbrigoImprovisado (
@@ -23,7 +26,8 @@ CREATE TABLE AbrigoImprovisado (
     id_usuario INT NOT NULL,
     endereco VARCHAR (300) NOT NULL,
     cep VARCHAR (8) NOT NULL,
-    capacidade INT DEFAULT 0,
+    capacidadeMaxima INT DEFAULT 0,
+    dataFuncionamento VARCHAR (16) NOT NULL,
 
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
--- banco-de-dados/dml.sql
+++ banco-de-dados/dml.sql
@@ -6,16 +6,19 @@ INSERT INTO Usuario (id, cpf, nome, email, senha, telefone) VALUES
 (1, '556.362.310-67', 'João da Silva', 'joao.dasilva@gmail.com', 'teste12345', '11987654321'),
 (2, '123.456.789-00', 'Maria Oliveira', 'maria.oliveira@hotmail.com', 'maria123', '21987654321');
 
-INSERT INTO AbrigoImprovisado (id, id_usuario, endereco, cep, capacidade) VALUES
-(1, 1, 'Rua das Flores, 123', '12345678', 10),
-(2, 1, 'Avenida Brasil, 456', '87654321', 5);
+INSERT INTO AbrigoImprovisado (id, id_usuario, endereco, cep, capacidadeMaxima, dataFuncionamento) VALUES
+(1, 1, 'Rua das Flores, 123', '12345678', 10, '05/12/2024 18:30'),
+(2, 1, 'Avenida Brasil, 456', '87654321', 5, '01/10/2024 20:30');
 
-INSERT INTO AbrigoOficial (id, nome, cnpj, endereco, cep, capacidade) VALUES
-(1, 'Abrigo Oficial São Paulo', '12.345.678/0001-90', 'Rua da Esperança, 789', '12345678', 50),
-(2, 'Abrigo Oficial Rio de Janeiro', '98.765.432/0001-10', 'Avenida do Sol, 321', '87654321', 30);
+INSERT INTO AbrigoOficial (id, nome, cnpj, endereco, cep, capacidadeMaxima, dataFuncionamento, email, telefone) VALUES
+(1, 'Abrigo Oficial São Paulo', '12.345.678/0001-90', 'Rua da Esperança, 789', '12345678',
+ 50, '01/09/2024 23:59', 'gov@email.com', '1199999999'),
+
+(2, 'Abrigo Oficial Rio de Janeiro', '98.765.432/0001-10', 'Avenida do Sol, 321', '87654321',
+ 30, '07/10/2024 11:59', 'prefeitura@email.com', '110000000');
 
 UPDATE Usuario SET senha = 'novaSenha123' WHERE id = 1;
-UPDATE AbrigoImprovisado SET capacidade = 15 WHERE id = 2;
+UPDATE AbrigoImprovisado SET capacidadeMaxima = 15 WHERE id = 2;
 
 DELETE FROM Usuario WHERE id = 2;
 DELETE FROM AbrigoImprovisado WHERE id = 2;
